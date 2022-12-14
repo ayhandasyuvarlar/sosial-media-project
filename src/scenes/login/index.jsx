e839d8c0bf6a04e0976ee916dca946b4611ff327
Ayhan Dasyuvarlar <ayhandasyuvarlar@gmail.com>
2022-12-14 20:37:08 +0300

    login page create

--- src/scenes/login/index.jsx
+++ src/scenes/login/index.jsx
@@ -1,5 +1,50 @@
+import { Typography, useMediaQuery, useTheme } from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
+import Form from "./form";
 
 export default function LoginPage() {
-  return <div>Login Page</div>;
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(max-width:1000px)");
+  const isNonMobileTwoScreens = useMediaQuery("(max-width:500px)");
+  console.log(isNonMobileScreens);
+  return (
+    <Box
+      display={"flex"}
+      flexDirection={isNonMobileScreens !== true ? "row" : "column"}
+      alignItems={"center"}
+      justifyContent={"space-around"}
+    >
+      <Box
+        width={isNonMobileScreens !== true ? "50%" : "100%"}
+        height={isNonMobileScreens !== true ? "100vh" : "50vh"}
+        p={"0.6rem"}
+        backgroundColor={theme.palette.background.alt}
+        alignItems={"center"}
+        display={"flex"}
+        justifyContent={"center"}
+      >
+        <Typography
+          fontWeight={"bold"}
+          fontSize={isNonMobileTwoScreens !== true ? "60px" : "50px"}
+          color={"primary"}
+          textAlign={"center"}
+        >
+          Welcome to Socipedia, the Social Media for Sociopaths!
+        </Typography>
+      </Box>
+      <Box
+        width={isNonMobileScreens !== true ? "50%" : "100%"}
+        height={isNonMobileScreens !== true ? "100vh" : "auto"}
+        p={"0.6rem"}
+        backgroundColor={theme.palette.background.alt}
+        display={"flex"}
+        flexDirection={"column"}
+        alignItems={"center"}
+        justifyContent={"center"}
+      >
+        <Form/>
+      </Box>
+    </Box>
+  );
 }
